09f135ff1e148320ae284eb12913f5756eccf2f9
Suraj <surajmondal888@gmail.com>
2021-01-15 12:43:52 +0530

    cancel tech interview

--- src/actions/interviewscheduler/updateinterviewschedule_action.js
+++ src/actions/interviewscheduler/updateinterviewschedule_action.js
@@ -2,12 +2,18 @@ import axios from 'axios'
 
 const UpdateInterviewScheduleAction = (props)=> {
     return async function(dispatch){
-        let error;
+        var err = null;
+        var onLoad = 0;
+        console.log(props);
         const axios = require('axios').default;
         const url = 'http://localhost:8080/cgits/interviewscheduler/'+props;
-        const serverResponse = await axios.get(url) //await can only be used with async function....
+        const serverResponse = await axios.put(url) //await can only be used with async function....
         .then(response => dispatch({type: 'UPDATE_INTERVIEW', payload: response}))
-        .catch(error => console.log(error));
+        .catch(error => console.log(err=error));
+        if(err!==null && props!=undefined){
+            console.log("No Interview Found");
+            dispatch({type: 'UPDATE_INTERVIEW', payload: null});
+        }
     }
 }
 
