f792fae7cdb9e719eb1594071b6b8c17d0010217
Sneha Chitale <snehachitale26@gmail.com>
2021-01-15 13:53:30 +0530

    reducer

--- src/actions/candidate/view_a_cand_hr.js
+++ src/actions/candidate/view_a_cand_hr.js
@@ -1,14 +1,20 @@
-//Action for viewing a candidate for tech panel
-const ViewCandidateForHrAction = (props) => {
-   
+//Action for viewing a candidate for hr panel
+import axios from 'axios'
+
+const ViewaCandidateForHRAction = (props)=> {
     return async function(dispatch){
-        let error;
+        var err = null;
+        var onLoad = 0;
         const axios = require('axios').default;
         const url = 'http://localhost:9091/cgits/candidate/hr/'+props;
         const serverResponse = await axios.get(url) //await can only be used with async function....
-        .then(response => dispatch({type: 'VIEW_CANDIDATE_FOR_HR', payload: response}))
-        .catch(error => console.log(error));
+        .then(response => dispatch({type: 'VIEW_A_CANDIDATE_FOR_HR', payload: response}))
+        .catch(error => console.log(err=error));
+        if(err!==null && props!=undefined){
+            console.log("No User Found");
+            dispatch({type: 'VIEW_A_CANDIDATE_FOR_HR', payload: null});
+        }
     }
- }
- 
- export default ViewCandidateForHrAction;
+}
+
+export default ViewaCandidateForHRAction;
