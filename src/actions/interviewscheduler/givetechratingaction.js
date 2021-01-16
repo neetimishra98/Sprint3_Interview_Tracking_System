3359f4ebe8a74c82cf2ce031e26e215fe4fbcadb
Sneha Chitale <snehachitale26@gmail.com>
2021-01-16 11:48:24 +0530

    action

--- src/actions/interviewscheduler/givetechratingaction.js
+++ src/actions/interviewscheduler/givetechratingaction.js
@@ -6,11 +6,8 @@ const GiveTechRatingAction = (props)=> {
         var err = null;
         var onLoad = 0;
         const axios = require('axios').default;
-<<<<<<< HEAD
-        const url = 'http://localhost:9090/cgits/interviewscheduler/tech/rating/'+props;
-=======
         const url = 'http://localhost:8080/cgits/interviewscheduler/tech/rating/'+props;
->>>>>>> 730d8b6aa3a41ccf407bd3dca6c0d061520c576c
+
         const serverResponse = await axios.get(url) //await can only be used with async function....
         .then(response => dispatch({type: 'GIVE_TECH_RATING', payload: response}))
         .catch(error => console.log(err=error));
